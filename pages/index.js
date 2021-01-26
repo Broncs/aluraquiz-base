33757312f44d7aea1ca4b0ec9396021ccbad4ed9
broncs <jhowfrombrazil@gmail.com>
2021-01-26 13:09:16 -0300

    end of second class

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,14 @@
-import styled from "styled-components";
-import db from "../db.json";
-import Widget from "../src/components/Widget";
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
 
-import GitHubCorner from "../src/components/GitHubCorner";
-import QuizBackground from "../src/components/QuizBackground";
-import Footer from "../src/components/Footer";
+import db from '../db.json';
+import Widget from '../src/components/Widget';
+
+import GitHubCorner from '../src/components/GitHubCorner';
+import QuizBackground from '../src/components/QuizBackground';
+import Footer from '../src/components/Footer';
 
 // const BackgroundImage = styled.div`
 //   background-image: url(${db.bg});
@@ -25,15 +29,66 @@ export const QuizContainer = styled.div`
 `;
 
 export default function Home() {
+  const router = useRouter();
+  const [name, setName] = useState('');
+
   return (
     <QuizBackground backgroundImage={db.bg}>
+      <Head>
+        {/* <!-- Primary Meta Tags --> */}
+        <title>Imersão Alura-Next.js</title>
+        <meta name="title" content="Imersão Alura-Next.js" />
+        <meta
+          name="description"
+          content="Projeto baseado em Quiz , Usando o Next.js com Styled-components"
+        />
+
+        {/* <!-- Open Graph / Facebook --> */}
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://metatags.io/" />
+        <meta property="og:title" content="Imersão Alura-Next.js" />
+        <meta
+          property="og:description"
+          content="Projeto baseado em Quiz , Usando o Next.js com Styled-components"
+        />
+        <meta
+          property="og:image"
+          content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"
+        />
+
+        {/* <!-- Twitter --> */}
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://metatags.io/" />
+        <meta property="twitter:title" content="Imersão Alura-Next.js" />
+        <meta
+          property="twitter:description"
+          content="Projeto baseado em Quiz , Usando o Next.js com Styled-components"
+        />
+        <meta
+          property="twitter:image"
+          content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"
+        />
+      </Head>
       <QuizContainer>
         <Widget>
           <Widget.Header>
             <h1>The legend of zelda</h1>
           </Widget.Header>
           <Widget.Content>
-            <p>lorem ipsum dolor sit amet....</p>
+            <form onSubmit={(e) => {
+              e.preventDefault();
+              router.push(`/quiz?name=${name}`);
+            }}
+            >
+              <input
+                onChange={(e) => {
+                  setName(e.target.value);
+                }}
+                placeholder="Diz ai seu nome"
+              />
+              <button type="submit" disabled={name.length === 0}>Jogar</button>
+            </form>
+
           </Widget.Content>
         </Widget>
         <Widget>
