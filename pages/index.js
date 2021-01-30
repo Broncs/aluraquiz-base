bcf01b586269ed60ee6fa4e91f7ccc261f0d1393
broncs <jhowfrombrazil@gmail.com>
2021-01-30 15:58:59 -0300

    Correcting MetaTag

--- pages/index.js
+++ pages/index.js
@@ -42,7 +42,7 @@ export default function Home() {
         />
         <meta
           property="og:image"
-          content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"
+          content="https://i.ibb.co/T1dspzM/QuizTag.png"
         />
 
         {/* <!-- Twitter --> */}
@@ -55,7 +55,7 @@ export default function Home() {
         />
         <meta
           property="twitter:image"
-          content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"
+          content="https://i.ibb.co/T1dspzM/QuizTag.png"
         />
       </Head>
       <QuizContainer>
@@ -95,7 +95,9 @@ export default function Home() {
                 placeholder="Diz ai seu nome"
                 value={name}
               />
-              {fillName && <p className="error-message">Por favor adicione um nome</p>}
+              {fillName && (
+                <p className="error-message">Por favor adicione um nome</p>
+              )}
               <Button type="submit" disabled={name.length === 0}>
                 {`Jogar ${name}`}
               </Button>
